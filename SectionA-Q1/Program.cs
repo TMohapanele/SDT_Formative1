e83484365307d5fd475b0e1ea1bd3fbfd9ece0b4
TSHIRE <168409719+GLOSSYYY@users.noreply.github.com>
2026-04-24 19:43:23 +0200

    Completed majority of question 1

--- SectionA-Q1/Program.cs
+++ SectionA-Q1/Program.cs
@@ -1,3 +1,26 @@
 ﻿//Question 1
 using System;
-    
+
+namespace SectionA_Q1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.Write("Enter student name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+
+            Console.Write("Enter mark for Subject 1: ");
+            int mark1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter mark for Subject 2: ");
+            int mark2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter mark for Subject 3: ");
+            int mark3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("=== STUDENT RESULTS ===");
+        }
+    } 
+}
