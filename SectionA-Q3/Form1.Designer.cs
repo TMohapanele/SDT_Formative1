94fe2ea146682f5109b4877e42067a6c25521249
TSHIRE <168409719+GLOSSYYY@users.noreply.github.com>
2026-04-26 19:46:23 +0200

    Completed Q3 code

--- SectionA-Q3/Form1.Designer.cs
+++ SectionA-Q3/Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            Languages = new ListBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            lblDate = new Label();
+            btnRemove = new Button();
+            btnAdd = new Button();
+            txtLanguage = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Languages
+            // 
+            Languages.BackColor = Color.WhiteSmoke;
+            Languages.FormattingEnabled = true;
+            Languages.Location = new Point(103, 79);
+            Languages.Name = "Languages";
+            Languages.Size = new Size(419, 164);
+            Languages.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(192, 0, 192);
+            label1.Location = new Point(114, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(396, 25);
+            label1.TabIndex = 1;
+            label1.Text = "My Favorite Programming Languages";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(255, 192, 255);
+            panel1.Controls.Add(lblDate);
+            panel1.Controls.Add(btnRemove);
+            panel1.Controls.Add(btnAdd);
+            panel1.Controls.Add(txtLanguage);
+            panel1.Controls.Add(Languages);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(71, 31);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(645, 386);
+            panel1.TabIndex = 2;
+            // 
+            // lblDate
+            // 
+            lblDate.AutoSize = true;
+            lblDate.Location = new Point(103, 348);
+            lblDate.Name = "lblDate";
+            lblDate.Size = new Size(0, 20);
+            lblDate.TabIndex = 5;
+            // 
+            // btnRemove
+            // 
+            btnRemove.BackColor = Color.FromArgb(255, 192, 192);
+            btnRemove.Location = new Point(239, 304);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(130, 29);
+            btnRemove.TabIndex = 4;
+            btnRemove.Text = "Remove";
+            btnRemove.UseVisualStyleBackColor = false;
+            btnRemove.Click += btnRemove_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = Color.FromArgb(192, 192, 255);
+            btnAdd.Location = new Point(103, 304);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(130, 29);
+            btnAdd.TabIndex = 3;
+            btnAdd.Text = "Add Language";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // txtLanguage
+            // 
+            txtLanguage.BackColor = Color.WhiteSmoke;
+            txtLanguage.Location = new Point(103, 258);
+            txtLanguage.Name = "txtLanguage";
+            txtLanguage.Size = new Size(419, 27);
+            txtLanguage.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 255);
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Favorite Programming Languages";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox Languages;
+        private Label label1;
+        private Panel panel1;
+        private TextBox txtLanguage;
+        private Button btnRemove;
+        private Button btnAdd;
+        private Label lblDate;
     }
 }
--- SectionA-Q3/Form1.cs
+++ SectionA-Q3/Form1.cs
@@ -6,5 +6,45 @@ namespace SectionA_Q3
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Prevent empty input
+            if (string.IsNullOrWhiteSpace(txtLanguage.Text))
+            {
+                MessageBox.Show("Please enter a programming language.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Prevent duplicate entries
+            if (Languages.Items.Contains(txtLanguage.Text))
+            {
+                MessageBox.Show("This programming language is already in the list.", "Duplicate Entry", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Add the language to the list
+            Languages.Items.Add(txtLanguage.Text);
+
+            //Show date and time
+            lblDate.Text = $"Added on: {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+
+            // Clear textbox
+            txtLanguage.Clear();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (Languages.SelectedItem != null)
+            {
+                Languages.Items.Remove(Languages.SelectedItem);
+                lblDate.Text = $"Removed on: {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+            }
+            else
+            {
+                MessageBox.Show("Please select a programming language to remove.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
     }
 }
