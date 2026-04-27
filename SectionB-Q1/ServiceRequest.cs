cb3a51b54ccbaaedbecd7af84da6f24b91fc2bd3
TSHIRE <168409719+GLOSSYYY@users.noreply.github.com>
2026-04-27 20:46:21 +0200

    Completed ServiceRequest.cs and began UtilitiesManager.cs

---
+++ SectionB-Q1/ServiceRequest.cs
@@ -0,0 +1,23 @@
+﻿public class ServiceRequest
+{
+    public string RequestType { get; set; }
+    public int Priority { get; set; }
+    public int Severity { get; set; }
+    public int EstimatedHours { get; set; }
+
+    public Resident Resident { get; set; } //Refers to Resident.cs 
+
+    public ServiceRequest(string type, int priority, int severity, int estimatedHours, Resident resident)
+    {
+        RequestType = type;
+        Priority = priority;
+        Severity = severity;
+        EstimatedHours = estimatedHours;
+        Resident = resident;
+    }
+
+    public int CalculateUrgency()
+    {
+        return Priority * Severity;
+    }
+}
---
+++ SectionB-Q1/UtilitiesManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class  UtilitiesManager
+{
+    Public List<ServiceRequest> Requests - new List<ServiceRequest>();
+
+    public void AddRequest(ServiceRequest request)
+    {
+        Requests.Add(request);
+    }
